bdad43e3e258c34951670bb2bb69c8450569ce57
rizadwiandhika <rizadwiandhika@gmail.com>
2022-02-25 10:36:29 +0700

    refactor: chage env override

--- myproject/settings.py
+++ myproject/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-environ.Env.read_env(env_file=BASE_DIR / '.env', overrides=True)
+environ.Env.read_env(env_file=BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
